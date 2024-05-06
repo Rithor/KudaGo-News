3577228f6012bb7ad79841c733ad3a73238e2af1
ViAnd <vitalii.andrianov@icloud.com>
2024-05-06 16:18:21 +0300

    webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env) => {
     },
     output: {
       filename: 'bundle.[contenthash].js',
-      path: path.resolve(__dirname, 'dist'),
+      path: env.mode ? path.resolve(__dirname, 'dist') : '/var/www/dist',
       publicPath: '/',
       clean: true,
     },
